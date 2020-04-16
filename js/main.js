4ea0bf0ecd04ef38b492c31dbd11cb370aaa6b75
Luigi <luigismilzo@live.com>
2020-04-16 17:06:35 +0200

    Add new task

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(document).ready( function() {
-    console.log('ok');
-    
+    var input = $('.task-new');
+    var taskList = $('.task-list');
+
+    input.keyup(function(e) {
+        if (e.which == 13 || e.keycode == 13) {
+            var item = $('.template li').clone();
+            item.prepend(input.val());
+            taskList.append(item);
+        }
+    });
+
+
 
 
 
